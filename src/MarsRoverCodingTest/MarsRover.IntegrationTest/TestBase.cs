6933e8283c7bb6150bd97405e75d515a3dea1f5e
Rojan Ulak <rulak@dart.org>
2018-11-09 17:12:04 -0600

    add integration test project

--- src/MarsRoverCodingTest/MarsRover.IntegrationTest/TestBase.cs
+++ src/MarsRoverCodingTest/MarsRover.IntegrationTest/TestBase.cs
@@ -1,42 +1,21 @@
-﻿namespace MarsRover.IntegrationTest
+﻿using System;
+using AutoMapper;
+using MarsRover.Data.Mapper;
+using MarsRover.Unity;
+
+namespace MarsRover.IntegrationTest
 {
-    public class TestBase
+    public class TestBase : IDisposable
     {
-        protected IUnityContainer container;
-        protected void RegisterAutoMapper()
-        {
-            AutoMapperConfig.Initiazlie();
-        }
+
         public void Setup()
         {
-            
-            RegisterAutoMapper();
-            RegisterContainer();
+            AutoMapperConfig.Initialize();
+            UnitySetup.RegisterComponents();
         }
-      
-        public void RegisterContainer()
+
+        public void Dispose()
         {
-
-            container = new UnityContainer();
-            container.RegisterTypes(
-                       AllClasses.FromLoadedAssemblies().Where(x => x.GetInterfaces().Contains(typeof(ISingletonDependency))),
-                       WithMappings.FromMatchingInterface,
-                       WithName.Default,
-                       WithLifetime.ContainerControlled
-                       );
-
-            container.RegisterTypes(
-           AllClasses.FromLoadedAssemblies().Where(x => x.GetInterfaces().Contains(typeof(ITransientDependency))),
-           WithMappings.FromMatchingInterface,
-           WithName.Default,
-           WithLifetime.Transient
-           );
-           
-
-        }
-        protected T Resolve<T>()
-        {
-            return container.Resolve<T>();
         }
     }
 }
