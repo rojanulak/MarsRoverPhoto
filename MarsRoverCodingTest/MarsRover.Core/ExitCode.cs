43e6d3ad98e91728a22f2dde9e9f2d103c13b9df
DeveloperPC\DeveloperX <ulak.rojan@live.com>
2018-11-08 18:58:51 -0600

    added Core Project

---
+++ MarsRoverCodingTest/MarsRover.Core/ExitCode.cs
@@ -0,0 +1,10 @@
+﻿namespace MarsRover.Core
+{
+    public enum ExitCode
+    {
+        Success = 0,
+        InvalidLogin = 1,
+        InvalidFilename = 2,
+        UnknownError = 10
+    }
+}
---
+++ MarsRoverCodingTest/MarsRover.Core/SystemClock.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace MarsRover.Core
+{
+    // Similar to https://github.com/HeadspringLabs/HeadspringTime/blob/master/SystemClock.cs
+    public static class SystemClock
+    {
+        private static Func<DateTime> now = nowDefault;
+        private static Func<DateTime> utcNow = utcNowDefault;
+
+        public static DateTime Now
+        {
+            get { return now(); }
+        }
+
+        public static DateTime UtcNow
+        {
+            get { return utcNow(); }
+        }
+
+        public static IDisposable StubNow(DateTime dateTime)
+        {
+            if (now != nowDefault)
+                throw new InvalidOperationException("Previous SystemClock.StubNow has not been disposed!");
+
+            now = () => dateTime;
+            return Disposable.Create(() => now = nowDefault);
+        }
+
+        public static IDisposable StubUtcNow(DateTime dateTime)
+        {
+            if (utcNow != utcNowDefault)
+                throw new InvalidOperationException("Previous SystemClock.StubUtcNow has not been disposed!");
+
+            utcNow = () => dateTime;
+            return Disposable.Create(() => utcNow = utcNowDefault);
+        }
+
+        private static DateTime nowDefault()
+        {
+            return DateTime.Now;
+        }
+
+        private static DateTime utcNowDefault()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
